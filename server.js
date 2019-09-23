4a9fb7a0e79551c26b80fc30d10ffb91195977fb
Marco Angel <angel2367716@gmail.com>
2019-09-22 20:15:48 -0700

    Updated the survey file to include initial html layout for survey questions

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require ('body-parser');
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3030
 
 const jsonParser = bodyParser.json(); //creates app/json parser
 const urlencodedParser = bodyParser.urlencoded({extended: false}); // creates application/x-www-form-urlencoded parser
@@ -14,8 +14,8 @@ app.use(bodyParser.raw({type: 'application/vnd.custom-type'}));//parse something
 app.use(bodyParser.text({type: 'text/html'})); //parses html body into a string
 
 //ROUTES
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
