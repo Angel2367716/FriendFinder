160bd947db22672d24e263cf9f556b7bbd89de0f
Marco Angel <angel2367716@gmail.com>
2019-09-26 03:13:33 -0700

    Added dummy data and it is being exported as friends

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+var friends = [
+    {
+        customerName: "Matt Johnston",
+        customerEmail: "https://images.pexels.com/photos/2974657/pexels-photo-2974657.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        scores: [
+            "1",
+            "2",
+            "2",
+            "4",
+            "5",
+            "3",
+            "4",
+            "2",
+            "5",
+            "2"
+        ]
+    }, 
+    {        customerName: "Amy Chambers",
+    customerEmail: "https://images.pexels.com/photos/2955180/pexels-photo-2955180.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    scores: [
+        "1",
+        "3",
+        "1",
+        "5",
+        "3",
+        "2",
+        "3",
+        "2",
+        "4",
+        "4"
+    ]}, 
+    {        customerName: "Barb Reid",
+    customerEmail: "https://images.pexels.com/photos/2961515/pexels-photo-2961515.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [
+        "2",
+        "3",
+        "3",
+        "2",
+        "5",
+        "4",
+        "3",
+        "3",
+        "4",
+        "2"
+    ]}, 
+    {        customerName: "David Ferguson",
+    customerEmail: "https://images.pexels.com/photos/2953859/pexels-photo-2953859.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [
+        "2",
+        "3",
+        "5",
+        "1",
+        "3",
+        "2",
+        "4",
+        "2",
+        "4",
+        "4"
+    ]},
+    {        customerName: "Rachel Lowe",
+    customerEmail: "https://images.pexels.com/photos/2960156/pexels-photo-2960156.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [
+        "4",
+        "2",
+        "2",
+        "4",
+        "4",
+        "3",
+        "5",
+        "2",
+        "5",
+        "5"
+    ]},
+    {        customerName: "Arnold",
+    customerEmail: "https://images.pexels.com/photos/2962816/pexels-photo-2962816.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [
+        "3",
+        "1",
+        "4",
+        "5",
+        "3",
+        "1",
+        "4",
+        "2",
+        "3",
+        "2"
+    ]},
+    {        customerName: "",
+    customerEmail: "",
+    scores: [
+        "5",
+        "5",
+        "4",
+        "2",
+        "1",
+        "4",
+        "3",
+        "5",
+        "3",
+        "2"
+    ]},
+];
+
+
+module.exports = friends;
