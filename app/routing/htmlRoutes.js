85e133ea602909ca7457c425392db1b34a22c991
Marco Angel <angel2367716@gmail.com>
2019-09-26 01:54:05 -0700

    Added submit button to survey html file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+const path = require("path");
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+// ---------------------------------------------------------------------------
+
+module.exports = function (app) {
+    app.get("/survey", function (req, res) {
+        res.sendfile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/home", function (req, res) {
+        res.sendfile(path.join(__dirname, "../public/home.html"));
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
